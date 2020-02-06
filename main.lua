72b90d039754c7bfece9f06b04081cbfa51e7923
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-02-05 22:37:27 -0800

    attempt at procedural wobble effect

--- main.lua
+++ main.lua
@@ -1,23 +1,80 @@
 local tove = require 'tove'
 
-local gradient = tove.newLinearGradient(120, 130, 140, 160)
-gradient:addColorStop(0, 0.8, 0.4, 0.6)
-gradient:addColorStop(1, 0.6, 0.2, 0.6)
-local myDrawing = tove.newGraphics()
-myDrawing:moveTo(100, 100)
-myDrawing:curveTo(120, 100, 210, 200, 200, 250)
-myDrawing:lineTo(50, 220)
-myDrawing:setFillColor(gradient)
-myDrawing:fill()
-myDrawing:setLineColor(0.2, 0.4, 0.6)
-myDrawing:setLineWidth(10)
-myDrawing:stroke()
+local function loadDrawing(path)
+    local drawing = tove.newGraphics(love.filesystem.newFileData(path):getString(), 100)
+    drawing:setResolution(2)
+    --drawing:setUsage('points', 'dynamic')
+    --drawing:setDisplay('mesh', 1024)
+    return drawing
+end
 
-local bigSvg = love.filesystem.newFileData('big.svg'):getString()
-local bigSvgDrawing = tove.newGraphics(bigSvg, 'copy')
+local drawing1 = loadDrawing('basketball-2.svg')
+local drawing2 = loadDrawing('cart-2.svg')
+local drawing3 = loadDrawing('hole-2.svg')
+local drawing4 = loadDrawing('soccer_goal-2.svg')
+
+local blocks = {}
+for i = 1, 18 do
+    blocks[i] = {
+        x = 200 + 400 * math.random(),
+        phase1 = 2 * math.pi * math.random(),
+        phase2 = 2 * math.pi * math.random(),
+        rate1 = 0.08 + 0.8 * (2 * math.random() - 1),
+        rate2 = 0.08 + 0.8 * (2 * math.random() - 1),
+        scale = 0.2 + 0.8 * math.random(),
+    }
+end
+
+local scribbles = setmetatable({}, { __mode = 'k' })
+
+local function scribbleDrawing(drawing)
+    scribbles[drawing] = scribbles[drawing] or {}
+    local s = scribbles[drawing]
+    for i = 1, drawing.paths.count do
+        for j = 1, drawing.paths[i].subpaths.count do
+            for k = 1, drawing.paths[i].subpaths[j].curves.count do
+                local curve = drawing.paths[i].subpaths[j].curves[k]
+                local cs = s[i .. '-' .. j .. '-' .. k]
+                if not cs then
+                    cs = {
+                        cp1x = curve.cp1x,
+                        cp1y = curve.cp1y,
+                        cp2x = curve.cp2x,
+                        cp2y = curve.cp2y,
+                    }
+                    s[i .. '-' .. j .. '-' .. k] = cs
+                end
+                curve.cp1x = 0.75 * (curve.cp1x + 1.8 * (2 * math.random() - 1)) + 0.25 * cs.cp1x
+                curve.cp1y = 0.75 * (curve.cp1y + 1.8 * (2 * math.random() - 1)) + 0.25 * cs.cp1y
+                curve.cp2x = 0.75 * (curve.cp2x + 1.8 * (2 * math.random() - 1)) + 0.25 * cs.cp2x
+                curve.cp2y = 0.75 * (curve.cp2y + 1.8 * (2 * math.random() - 1)) + 0.25 * cs.cp2y
+            end
+        end
+    end
+end
 
 function love.draw()
-    myDrawing:draw()
+    love.graphics.push('all')
+
+    scribbleDrawing(drawing1)
+    local t = love.timer.getTime()
+    for _, block in ipairs(blocks) do
+        love.graphics.push()
+        love.graphics.translate(block.x, 0.5 * 450 * (math.sin(block.phase1 + block.rate1 * t) + 1))
+        love.graphics.rotate(block.phase2 + block.rate2 * t)
+        love.graphics.scale(block.scale)
+        drawing1:draw()
+        love.graphics.pop()
+    end
+    drawing1:draw(0.25 * 800, 0.25 * 450)
+    scribbleDrawing(drawing2)
+    drawing2:draw(0.75 * 800, 0.25 * 450)
+    scribbleDrawing(drawing3)
+    drawing3:draw(0.25 * 800, 0.75 * 450)
+    scribbleDrawing(drawing4)
+    drawing4:draw(0.75 * 800, 0.75 * 450)
+
+    love.graphics.pop('all')
 
     love.graphics.print('fps: ' .. love.timer.getFPS(), 20, 20)
 end
